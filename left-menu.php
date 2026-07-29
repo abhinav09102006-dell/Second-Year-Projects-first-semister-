8c68194620d7f39c76038aaf3fc69a2bedfd83fa
Abhinav <abhinav09102006@gmail.com>
2026-07-29 19:58:44 +0530

    Add files via upload

---
+++ left-menu.php
@@ -0,0 +1,39 @@
+<div class="left-side-section-account">
+    <div class="uder-box-p">
+        <span><i class="fa-solid fa-user"></i></span>
+        <h5>Hi, Akash bhardwaj</h5>
+    </div>
+    <ul class="tabs">
+        <li class="tab">
+            <a class="active" href="#">
+                <i class="fa-solid fa-user"></i>
+                <span class="tabMargin-new">My Account</span></a>
+        </li>
+        <li class="tab">
+            <a href="#">
+                <i class="fa-solid fa-bag-shopping"></i>
+                <span class="tabMargin">My Orders</span></a>
+        </li>
+        <li class="tab">
+            <a href="#">
+                <i class="fa-solid fa-location-crosshairs"></i>
+                <span class="tabMargin-new">My Address Book</span></a>
+        </li>
+        <li class="tab">
+            <a href="#">
+                <i class="fa-solid fa-wallet"></i>
+                <span class="tabMargin">My Wallet</span></a>
+        </li>
+        <li class="tab">
+            <a href="#">
+                <i class="fa-solid fa-money-bill"></i>
+                <span class="tabMargin">Coupons</span></a>
+        </li>
+        <li class="tab">
+            <a href="#">
+                <i class="fa-solid fa-bell"></i>
+                <span class="tabMargin-new">Reminder</span></a>
+        </li>
+    </ul>
+
+</div>
